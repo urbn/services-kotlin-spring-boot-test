872a45a62383249041e96c56ce7afeed61c4f28e
Fawad Rafi <fawadrafi@propelsity.com>
2018-11-13 12:56:04 -0500

    Do not process orders with email address because it is a required field

--- src/main/kotlin/com/urbn/rewards/controller/RewardsController.kt
+++ src/main/kotlin/com/urbn/rewards/controller/RewardsController.kt
@@ -4,10 +4,12 @@ import com.urbn.rewards.models.Customer
 import com.urbn.rewards.models.OrderRequest
 import com.urbn.rewards.models.Rewards
 import com.urbn.rewards.service.OrderService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.ResponseStatus
 
 // RestController denotes that this class has web functionality
 // The constructor value (orderService) is initialized through dependency injection (Spring magic)
@@ -16,6 +18,11 @@ class RewardsController(private val orderService: OrderService) {
 
     @PostMapping("/purchase")
     fun purchase(@RequestBody orderRequest: OrderRequest): Map<String, Customer> {
+		// Do not process orders without an email
+		if (orderRequest.email.isNullOrEmpty()) {
+			throw InvalidOrderException("No email provided.")
+		}
+			
         // right now the orderService returns a map of all customers once created.
         // TASK: we should return only the customer that made the request
         return orderService.purchase(orderRequest).filterKeys { it == orderRequest.email }
@@ -26,4 +33,7 @@ class RewardsController(private val orderService: OrderService) {
         return orderService.rewards
     }
 }
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+class InvalidOrderException(message: String) : Exception(message)
 
