ba503e32d76f650f2ce9ff033689ff070b84483c
Torning, Fraser <Fraser_Torning@comcast.com>
2019-06-03 10:56:30 -0400

    purchase endpoint logic created

--- src/main/kotlin/com/urbn/rewards/controller/RewardsController.kt
+++ src/main/kotlin/com/urbn/rewards/controller/RewardsController.kt
@@ -4,25 +4,34 @@ import com.urbn.rewards.models.Customer
 import com.urbn.rewards.models.OrderRequest
 import com.urbn.rewards.models.Rewards
 import com.urbn.rewards.service.OrderService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 // RestController denotes that this class has web functionality
 // The constructor value (orderService) is initialized through dependency injection (Spring magic)
 @RestController
 class RewardsController(private val orderService: OrderService) {
 
+    // **Endpoint 1**
     @PostMapping("/purchase")
-    fun purchase(@RequestBody orderRequest: OrderRequest): Map<String, Customer> {
+    fun purchase(@RequestBody orderRequest: OrderRequest): Customer? {
         // right now the orderService returns a map of all customers once created.
         // TASK: we should return only the customer that made the request
-        return orderService.purchase(orderRequest).filterKeys {
-            it == orderRequest.email
-        }
+        return orderService.purchase(orderRequest)
     }
 
+    // **Endpoint 2**
+    @GetMapping("/customer/{email}")
+    fun getCustomerRewards(@PathVariable email: String): Customer? {
+        return orderService.getCustomer(email)
+    }
+
+    // **Endpoint 3**
+    @GetMapping("/customers")
+    fun getAllCustomerRewards(): List<Customer>? {
+        return orderService.getCustomers()
+    }
+
+    // existing
     @GetMapping("/rewards")
     fun getRewardsTiers(): Array<Rewards> {
         return orderService.rewards
--- src/main/kotlin/com/urbn/rewards/service/OrderService.kt
+++ src/main/kotlin/com/urbn/rewards/service/OrderService.kt
@@ -23,20 +23,111 @@ class OrderService {
         rewards = gson.fromJson(rewardString, Array<Rewards>::class.java)
     }
 
-    fun purchase(orderRequest: OrderRequest): Map<String, Customer> {
+    fun purchase(orderRequest: OrderRequest): Customer? {
         // Implement purchase endpoint logic here
         // Right now, we're only storing the customer into a hash map
 
+        // declare the variables and set to base case
+        var newRewardPoints = Math.floor(orderRequest.purchaseTotal.toDouble()).toInt()
+        var newRewardsTier = ""
+        var newNextRewardsTier = "A"
+        var newNextRewardsTierName = "5% off purchase"
+        var newNextRewardsTierProgress = (newRewardPoints / 100.0).toFloat()
+
+        // check if the customer exists and if so, add existing reward points
+        if (customers[orderRequest.email] != null) {
+            val existingCustomer = customers[orderRequest.email]
+            // this took some googling to figure out null case
+            newRewardPoints += existingCustomer?.rewardPoints ?:0
+        }
+
+        // with the new total, round down to nearest tier point level
+        val truncPointsToTier = newRewardPoints - (newRewardPoints % 100)
+
+        // check if max level is already reached
+        if (truncPointsToTier in 100..999) {
+            for ( reward in rewards ) {
+                if ( reward.points == truncPointsToTier ) {
+                    newRewardsTier = reward.tier
+                    newNextRewardsTierProgress = (( newRewardPoints - truncPointsToTier ) / 100.0).toFloat()
+
+                } else if ( reward.points == truncPointsToTier + 100 ) {
+                    newNextRewardsTier = reward.tier
+                    newNextRewardsTierName = reward.rewardName
+                }
+            }
+        } else if (truncPointsToTier >= 1000) {
+            newRewardsTier = "J"
+            newNextRewardsTierProgress = 0.0F
+            newNextRewardsTier = ""
+            newNextRewardsTierName = ""
+        }
+
+        // set the customer key to the new values
         customers[orderRequest.email] = Customer(
-            email = orderRequest.email,
-            rewardPoints = Math.floor(orderRequest.purchaseTotal.toDouble()).toInt(),
-            nextRewardsTier = "??",
-            rewardsTier = "???",
-            nextRewardsTierName = "???",
-            nextRewardsTierProgress = 0.0.toFloat()
+                email = orderRequest.email,
+                rewardPoints = newRewardPoints,
+                nextRewardsTier = newNextRewardsTier,
+                rewardsTier = newRewardsTier,
+                nextRewardsTierName = newNextRewardsTierName,
+                nextRewardsTierProgress = newNextRewardsTierProgress
         )
 
-        return customers
+        // return the customer
+        return getCustomer(orderRequest.email)
+
+//        // get the current customer
+//        val currentCustomer = customers[orderRequest.email]
+//
+//        // just realized I can't'
+////        currentCustomer.rewardPoints += Math.floor(orderRequest.purchaseTotal.toDouble()).toInt(),
+////        return currentCustomer
+//
+//        customers[orderRequest.email] = Customer(
+//                email = orderRequest.email,
+//                rewardPoints = 0,
+//                nextRewardsTier = "",
+//                rewardsTier = "",
+//                nextRewardsTierName = "",
+//                nextRewardsTierProgress = 0.0.toFloat()
+//        )
+//
+//        return getCustomer(orderRequest.email)
+
+    }
+//
+//        totalRewardPoints += (existingCustomer?.rewardPoints ?: 0)
+//
+//        var rewardPoints = ""
+//        var nextRewardsTier = ""
+//        var rewardsTier = ""
+//        var nextRewardsTierName = ""
+//        var nextRewardsTierProgress: Float
+//
+//        fun rewardPoints(): Int {
+//
+//        }
+//
+//        customers[orderRequest.email] = Customer(
+//            email = orderRequest.email,
+//            rewardPoints = Math.floor(orderRequest.purchaseTotal.toDouble()).toInt(),
+//            nextRewardsTier = "??",
+//            rewardsTier = "???",
+//            nextRewardsTierName = "???",
+//            nextRewardsTierProgress = 0.0.toFloat()
+//        )
+//
+//        return customers
+//    }
+
+    // adding functions for new endpoints
+
+    fun getCustomers(): List<Customer>? {
+        return customers.values.toList()
+    }
+
+    fun getCustomer(email: String): Customer? {
+        return customers[email]
     }
 
     private fun getRewards(): String {
